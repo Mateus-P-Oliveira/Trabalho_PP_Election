aedb5459bcbaddaaeab6d69b83194767bd6b8b55
Mateus-P-Oliveira <mateus.pinto@acad.pucrs.br>
2023-05-20 19:05:47 +0000

    Fix Print

--- election_practice.go
+++ election_practice.go
@@ -35,7 +35,8 @@ func ElectionControler(in chan int) {
 	//fmt.Printf("consome %d %d\n", randomErrorNode, randomErrorChannel)
 	// mudar o processo 0 - canal de entrada 3 - para falho (defini mensagem tipo 2 pra isto)
 
-	temp.tipo = randomErrorNode //1 e 4 e 5 se forem atribuidos dão erro
+	temp.tipo = randomErrorNode
+	//randomErrorChannelSave := randomErrorChannel
 	//Aloca Valores dos Aneis que não forem os escolhidos-------------------
 	switch randomErrorChannel {
 	case 0:
@@ -73,39 +74,24 @@ func ElectionControler(in chan int) {
 		{
 			temp.tipo = 99 //Mensagem generica
 			chans[0] <- temp
-			fmt.Printf("%d\n", <-in) //Controle de Confirmação
+			fmt.Printf("\n", <-in) //Controle de Confirmação
 			chans[1] <- temp
-			fmt.Printf("%d\n", <-in) //Controle de Confirmação
+			fmt.Printf("\n", <-in) //Controle de Confirmação
 			chans[2] <- temp
-			fmt.Printf("%d\n", <-in) //Controle de Confirmação
+			fmt.Printf("\n", <-in) //Controle de Confirmação
 		}
 	default:
 		{
 			fmt.Printf("Nodo não existe \n")
 		}
 	}
+	temp.tipo = randomErrorNode
 	//Não posso deixar de atribuir valor pros Channels
 	chans[randomErrorChannel] <- temp
 	fmt.Printf("Nodo: %d Recebeu: %d\n", randomErrorChannel, randomErrorNode)
-
 	fmt.Printf("Controle: confirmação %d\n", <-in) // receber e imprimir confirmação
 
 	// mudar o processo 1 - canal de entrada 0 - para falho (defini mensagem tipo 2 pra isto)
-
-	/*temp.tipo = randomErrorNode
-	chans[0] <- temp
-	fmt.Printf("Controle: mudar o processo 1 para falho\n")
-	fmt.Printf("Controle: confirmação %d\n", <-in) // receber e imprimir confirmação
-
-	// matar os outrs processos com mensagens não conhecidas (só pra cosumir a leitura)
-
-	temp.tipo = randomErrorNode //Não podem enviar para o mesmo tipo de mensagem sem o imprimir confirmação
-	chans[1] <- temp
-	fmt.Printf("Controle: confirmação %d\n", <-in) // receber e imprimir confirmação //Precisa dele para não dar deadlock
-	temp.tipo = randomErrorNode
-	chans[2] <- temp
-	fmt.Printf("Controle: confirmação %d\n", <-in) // receber e imprimir confirmação
-	*/
 
 	fmt.Println("\n   Processo controlador concluído\n")
 }
@@ -122,17 +108,17 @@ func ElectionStage(TaskId int, in chan mensagem, out chan mensagem, leader int) 
 
 	temp := <-in // ler mensagem
 	//Corpo é o vetor de votação e precisa inclunir todos valores de preocessos que pertencem ao anel
-	fmt.Printf("%2d: recebi mensagem %d, [ %d, %d, %d ]\n", TaskId, temp.tipo, temp.corpo[0], temp.corpo[1], temp.corpo[2])
+
 	switch temp.tipo {
 	case 1:
 		{
 			fmt.Printf("Caso 1 \n")
-
+			fmt.Printf("%2d: recebi mensagem %d, [ %d, %d, %d ]\n", TaskId, temp.tipo, temp.corpo[0], temp.corpo[1], temp.corpo[2])
 			controle <- -5
 		}
 	case 2:
 		{
-			//fmt.Printf("%2d: recebi mensagem %d, [ %d, %d, %d ]\n", TaskId, temp.tipo, temp.corpo[0], temp.corpo[1], temp.corpo[2])
+			fmt.Printf("%2d: recebi mensagem %d, [ %d, %d, %d ]\n", TaskId, temp.tipo, temp.corpo[0], temp.corpo[1], temp.corpo[2])
 			bFailed = true
 			fmt.Printf("%2d: falho %v \n", TaskId, bFailed)
 			fmt.Printf("%2d: lider atual %d\n", TaskId, actualLeader)
@@ -141,7 +127,7 @@ func ElectionStage(TaskId int, in chan mensagem, out chan mensagem, leader int) 
 	case 3:
 		{
 
-			//fmt.Printf("%2d: recebi mensagem %d, [ %d, %d, %d ]\n", TaskId, temp.tipo, temp.corpo[0], temp.corpo[1], temp.corpo[2])
+			fmt.Printf("%2d: recebi mensagem %d, [ %d, %d, %d ]\n", TaskId, temp.tipo, temp.corpo[0], temp.corpo[1], temp.corpo[2])
 			bFailed = false
 			fmt.Printf("%2d: falho %v \n", TaskId, bFailed)
 			fmt.Printf("%2d: lider atual %d\n", TaskId, actualLeader)
@@ -149,13 +135,13 @@ func ElectionStage(TaskId int, in chan mensagem, out chan mensagem, leader int) 
 		}
 	case 4:
 		{
-			//fmt.Printf("%2d: recebi mensagem %d, [ %d, %d, %d ]\n", TaskId, temp.tipo, temp.corpo[0], temp.corpo[1], temp.corpo[2])
+			fmt.Printf("%2d: recebi mensagem %d, [ %d, %d, %d ]\n", TaskId, temp.tipo, temp.corpo[0], temp.corpo[1], temp.corpo[2])
 			fmt.Printf("Caso 4 \n")
 			controle <- -5
 		}
 	case 5:
 		{
-			//fmt.Printf("%2d: recebi mensagem %d, [ %d, %d, %d ]\n", TaskId, temp.tipo, temp.corpo[0], temp.corpo[1], temp.corpo[2])
+			fmt.Printf("%2d: recebi mensagem %d, [ %d, %d, %d ]\n", TaskId, temp.tipo, temp.corpo[0], temp.corpo[1], temp.corpo[2])
 			fmt.Printf("Caso 5 \n")
 			controle <- -5
 		}
